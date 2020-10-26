225460a6cf8c368e1f53099624cb13f98a7c1e61
Joseph Spratt <joseph.spratt@makepositive.com>
2020-10-26 15:49:45 +0000

    added images and css

--- assets/js/githubUI.js
+++ assets/js/githubUI.js
@@ -5,6 +5,7 @@ class UI {
 
     showProfile(user){
         console.log(user);
+        let date = new Date(user.created_at);
         this.profile.innerHTML = `
             <div class="card card-body mb-3">
                 <div class="row>
@@ -23,7 +24,7 @@ class UI {
                             <li class="list-group-item">Company: ${user.company} </li>
                             <li class="list-group-item">Website/Blog: ${user.blog} </li>
                             <li class="list-group-item">Location: ${user.location} </li>
-                            <li class="list-group-item">Member Since: ${user.created_at} </li>
+                            <li class="list-group-item">Member Since: ${date.toDateString()} </li>
                         </ul>
                     </div>
                 </div>
@@ -58,7 +59,7 @@ class UI {
                         <div class="col md-6">
                             <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
                             <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
                             <span class="badge badge-primary">Forks: ${repo.forks_count}</span>
